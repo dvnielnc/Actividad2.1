14e05205160980a71cb80f0c4a3117026062a33e
dvnielnc <djopia863.a@gmail.com>
2024-09-25 16:19:29 -0300

    1ra version actividad 2.1

---
+++ app/src/main/java/com/example/actividad21/Inicio.java
@@ -0,0 +1,41 @@
+package com.example.actividad21;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_inicio);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        final Button btnAceptar = (Button) findViewById(R.id.btnAceptar);
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editTextNombre = (EditText) findViewById(R.id.editTextNombre);
+                String StNombre = editTextNombre.getText().toString();
+            }
+        });
+
+        Intent sIntent = new Intent(Inicio.this,Resultado.class);
+        sIntent.putExtra("STNombre",StNombre);
+        startActivity(sIntent);
+    }
+}
